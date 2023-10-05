8a4ea71b1727b5033b667de2333c2ad22c54fdaa
dhdl2389 <dhdl2389@naver.com>
2023-10-05 16:03:56 +0900

    20231005

--- day1/src/main/java/com/acorn/day1/BController.java
+++ day1/src/main/java/com/acorn/day1/BController.java
@@ -5,23 +5,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-// 매서드 단위로 매핑을 할 수 있다.
+// 留ㅼ꽌�뱶 �떒�쐞濡� 留ㅽ븨�쓣 �븷 �닔 �엳�떎.
 @Controller
 public class BController {
-	
+
 	@RequestMapping(value = "/test1", method = RequestMethod.GET)
 	public String test1() {
-		// 반환타입 String => view 이름 의미함
+		// 諛섑솚���엯 String => view �씠由� �쓽誘명븿
 		return "viewName";
 	}
-	
+
 	@RequestMapping(value = "/test2", method = RequestMethod.GET)
 	public void test2() {
-		// void일 때는 mapping정보를 view이름으로 인식함
+		// void�씪 �븣�뒗 mapping�젙蹂대�� view�씠由꾩쑝濡� �씤�떇�븿
 	}
-	
+
+//한글 
 	@GetMapping("/test3")
 	public void test3() {
-		
+
 	}
 }
