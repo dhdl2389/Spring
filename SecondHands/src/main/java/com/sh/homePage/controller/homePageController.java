d5019fd70eca78c6b41d6d67f6a4025689adf236
dhdl2389 <dhdl2389@naver.com>
2023-11-22 17:48:13 +0900

    20231122

--- SecondHands/src/main/java/com/sh/homePage/controller/homePageController.java
+++ SecondHands/src/main/java/com/sh/homePage/controller/homePageController.java
@@ -3,10 +3,8 @@ package com.sh.homePage.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class homePageController {
@@ -24,12 +22,5 @@ public class homePageController {
 
 		return "/homePage/homePage";
 	}
-	
-	@RequestMapping("/count")
-	public String count(HttpSession session, Model model) {        
-	    Integer userCount = (Integer) session.getServletContext().getAttribute("userCount");
-	    model.addAttribute("userCount", userCount);
-	    return "/homePage/homePage";
-	}
-	
+
 }
--- SecondHands/src/main/java/com/sh/login/controller/LogoutController.java
+++ SecondHands/src/main/java/com/sh/login/controller/LogoutController.java
@@ -10,8 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class LogoutController {
 
 	@PostMapping("/logout")
-	public String logout(HttpSession s) {
-		s.invalidate();		
+	public String logout(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			session.invalidate(); // 세션 무효화
+		}
 		return "/homePage/homePage"; // 로그아웃 성공 페이지 또는 로그인 페이지로 리다이렉트
 	}
 }
